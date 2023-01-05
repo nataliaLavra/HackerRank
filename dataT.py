ee94262d8ef25c2e50f00c4abd219f2c5e64727d
Natália Lavra <54447974+nataliaLavra@users.noreply.github.com>
2023-01-05 11:03:50 -0300

    Create dataT.py

---
+++ dataT.py
@@ -0,0 +1,15 @@
+# Declare second integer, double, and String variables.
+i2 = 12
+d2 = 4.0
+s2 = 'is the best place to learn and practice coding!'
+# Read and save an integer, double, and String to your variables.
+i3 = int(input())
+d3 = float(input())
+s3 = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+i3)
+# Print the sum of the double variables on a new line.
+print(d+d3)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+s3)
